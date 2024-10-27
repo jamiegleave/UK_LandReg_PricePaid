1868ee90cb689d4e02ec3505ef7af77bb26c407c
jamiegleave <jamie.gleave.xbox@gmail.com>
2024-10-27 14:57:41 +0000

    v1.2

--- Docker/initalisation.py
+++ Docker/initalisation.py
@@ -1,5 +1,3 @@
-import os
-from glob import glob
 import requests
 import subprocess
 from tqdm import tqdm
@@ -7,44 +5,30 @@ from datetime import datetime as dt
 import mysql.connector
 
 
-def define_name():
-    return str(dt.today().day)+str(dt.today().month)+str(dt.today().year % 100)+'pricepaidcomplete'
+def get_req(url):
+    response = requests.get(url,stream=True)
+    last_mod = dt.strptime(response.headers.get('Last-Modified'),'%a, %d %b %Y %H:%M:%S %Z')
+    return {
+        'response': response,
+        'last_mod': last_mod,
+        'file_name': last_mod.strftime('%b')+str(last_mod.strftime('%y'))+'pricepaidcomplete',
+        'file_sz':int(response.headers.get('content-length',0))
+    }
 
 
 def fetch_csv(url, dest):
-    file_name = define_name()
-
-    response = requests.get(url,stream=True)
-
-    total_size = int(response.headers.get('content-length',0))
+    resp_dict = get_req(url)
+    
     block_size = 1024
-
-    with tqdm(total=total_size,unit='B',unit_scale=True) as progress_bar:
-        with open(dest+file_name+'.csv','wb') as file:
-            for chunk in response.iter_content(block_size):
+    
+    with tqdm(total=resp_dict['file_sz'],unit='B',unit_scale=True) as progress_bar:
+        with open(dest+resp_dict['file_name']+'.csv','wb') as file:
+            for chunk in resp_dict['response'].iter_content(block_size):
                 progress_bar.update(len(chunk))
                 file.write(chunk)
             file.flush()
 
-
-def check_vers(url, dest):
-    response = requests.get(url,stream=True)
-    server_sz = int(response.headers.get('content-length',0))
-    
-    print('Server file is'+' '+str(round(server_sz/1000000000,5))+' '+'GB')
-
-    try:
-        local_sz = os.path.getsize(max(glob(dest+'*'),key=os.path.getctime))
-        print('Local file is'+' '+str(round(local_sz/1000000000,5))+' '+'GB')
-        if local_sz == server_sz:
-            print('Local file is up-to-date')
-        else:
-            print('Updated version available on server - initiating download')
-            fetch_csv(url,dest)
-            
-    except:
-        print('No file exists - initiating download')
-        fetch_csv(url,dest)
+    return resp_dict['file_name']
 
 
 def copy_file_to_docker_volume(file_name, container_name, local_path, mount_path):
@@ -55,11 +39,12 @@ def copy_file_to_docker_volume(file_name, container_name, local_path, mount_path
     print(f"Successfully copied {local_path} to path {mount_path} (container {container_name})")
 
 
-def sql_commitchanges(query):
-    cnx = mysql.connector.connect(user='root',password='password',
-                              host = 'localhost',
-                              port = 3306,
-                              database='CSV_DB 7')
+def sql_commitchanges(query, user,password,host,port,database):
+    cnx = mysql.connector.connect(user=user,
+                                  password=password,
+                                  host=host,
+                                  port=port,
+                                  database=database)
     
     cursor = cnx.cursor(buffered=True)
     cursor.execute(query)
